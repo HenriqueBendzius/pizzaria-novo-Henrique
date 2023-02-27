1716c2425a8a04496741a9292a29b6071bec597e
Henrique Bendzius <vendas.henriquebolsas@gmail.com>
2023-02-27 11:19:34 -0300

    add middelware

---
+++ middlewares/bloqueiaForaDeHora.js
@@ -0,0 +1,24 @@
+
+
+// Criar middleware bloqueiaForaDeHora
+
+// Verificar se hora é maior que 0 e menor 19
+
+// Nesse caso, mandar mensagem de "Volte mais tarde"
+
+// Caso contrário, deixar req ir adiante...
+
+
+const bloqueiaForaDeHora =(req, res, next)=>{
+
+    let hora = (new Date()).getHours();
+
+    if(hora <= 10){ 
+        res.send("volte mais tarde");
+
+    }else {
+        next();
+    }
+}
+
+module.exports = bloqueiaForaDeHora;
