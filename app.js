745dabfd45274177192968c72a81e7183d9b937c
Henrique Bendzius <vendas.henriquebolsas@gmail.com>
2023-03-06 11:20:59 -0300

    aula 06/03 criando rotas, session , cookies

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // 1 - Importar o express
 const express = require('express');
 const path = require('path');
+const session =require('express-session');
+
 const bloqueiaForaDeHora = require('./middlewares/bloqueiaForaDeHora');
 const registraRequisicao = require('./middlewares/registraRequisicao');
 const router = require('./router');
@@ -10,6 +12,13 @@ const servidor = express();
 servidor.set('view engine','ejs');
 
 // Define a pasta public como sendo a pasta arquivos estáticos
+servidor.use(session({
+    secret: 'SEGREDO',
+    resave: false,
+    saveUninitialized: false
+}))
+
+
 servidor.use(express.static(path.join(__dirname, 'public')))
 servidor.use(express.urlencoded({ extended: false }));
 
--- controllers/AdmController.js
+++ controllers/AdmController.js
@@ -1,5 +1,6 @@
 const PizzasServices = require("../services/PizzasServices");
 const fs = require('fs')
+const bcrypt = require('bcrypt');
 
 const AdmController = {
     listarPizzas: (req, res) =>{
@@ -49,7 +50,47 @@ const AdmController = {
 
         // Renderizar a view (ainda inexistente) form-edit-pizza.ejs
         // passando para essa view (res.render(____, {pizza}))
+    },
+
+    
+    showLogin: (req, res) => {
+        res.render('login.ejs');
+    },
+    login: (req, res) => {
+        // res.send(req.body);
+        // 1 - Capturar o email e a senha digitados pelo administrador
+       const {email, senha}=  req.body;
+       
+
+        // 2 - Verificar a existência do administrador.
+        // Caso não exista, enviar mensagem de erro
+        const administradores = require('../databases/administradores.json');
+        let adm = administradores.find(adm =>adm.email === email);
+        if(adm === undefined){
+           return res.send("falha no login");
+
+        }
+
+        // 3 - Verificar a senha do administrador.
+        // Caso senha não seja válida, enviar mensagem de erro
+        const senhaOk = bcrypt.compareSync(senha, adm.senha);
+        if(!senhaOk){
+            return res.send("falha no login");
+        }
+        // res.send('Login ok ... vamos prosseguir para proximos passos!');
+
+
+        // 4 - Criar a session/cookie do administrador
+        req.session.admLogado =true;
+
+
+        // 5 - Redirecioná-lo para /adm/pizzas
+        res.redirect('/adm/pizzas');
+
+
     }
+
+
 
 }
 
